357c5ae68ed6e8dbd0b50fc4516e66789a19cc5d
Ariel <arielord@buffalo.edu>
2019-04-01 21:35:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     end
     post '/teams' do
       @teams = params[:team]
-      @heroes = params[:team][:member]
+      @heroes = params[:team][:members]
       erb :team
     end
 
